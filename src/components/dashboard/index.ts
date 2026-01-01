aa65d86d802cdefadb94f74bf5e2c6742b555b07
wishtree-rpatel <rajkumar.patel@wishtreetech.com>
2026-01-01 23:19:42 +0530

    Dashboard components created

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Dashboard Components
+ * Export all dashboard components for easy imports
+ */
+
+export { MetricCard } from './MetricCard';
+export type { MetricCardProps } from './MetricCard';
+
+export { DateRangeFilter } from './DateRangeFilter';
+export type { DateRangeFilterProps } from './DateRangeFilter';
+
+export { ActivityTimelineItem } from './ActivityTimelineItem';
+export type { ActivityTimelineItemProps } from './ActivityTimelineItem';
+
+export { UpcomingDueItem } from './UpcomingDueItem';
+export type { UpcomingDueItemProps } from './UpcomingDueItem';
